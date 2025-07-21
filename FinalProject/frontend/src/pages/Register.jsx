036a65cbd6b3a62c33727cedce68a5532cd6be03
Don Melson <donmelson1@gmail.com>
2025-07-21 23:55:15 +0300

    Update Register.jsx

--- FinalProject/frontend/src/pages/Register.jsx
+++ FinalProject/frontend/src/pages/Register.jsx
@@ -38,7 +38,7 @@ function Register({ setIsLoggedIn }) {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/register', {
+      const res = await axios.post('https://tech-frontier-2-3vgy.onrender.com/register', {
         fullName: name,
         email,
         password
